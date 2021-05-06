41ece6e1e3a486fb2f2f3f9057ad4d74d16322b1
Kronapsys <rsxvii@gmail.com>
2021-05-06 21:33:41 +0200

    added btn crear evento to mainMenuTeachers

--- src/containers/MainMenuTeachers/MainMenuTeachers.jsx
+++ src/containers/MainMenuTeachers/MainMenuTeachers.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import './MainMenuTeachers.css';
-import ChildCard from '../../components/ChildCard/ChildCard';
+import React from "react";
+import "./MainMenuTeachers.css";
+import ChildCard from "../../components/ChildCard/ChildCard";
+import Btn from "../../components/Btn/Btn";
 
 const MainMenuTeachers = () => {
-    return(
+  return (
     
-        <div className='MainMenuTeachers'>
-            <div className="areaMMT">
-                <ChildCard/>
+    <div className="MainMenuTeachers">
+      <div className="areaMMT">
+        <ChildCard />
+        <ChildCard />
 
-
-
-            </div>
-            
+        <div className="eventArea">
+          <Btn nombre="Crear evento" destination="" />
         </div>
-    
-    );
+      </div>
+    </div>
+
+  );
 };
 
 export default MainMenuTeachers;
